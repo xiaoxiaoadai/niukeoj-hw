7fc82317fb86e6151b4c5d0a485a5e3e51aff374
caozuohua <caozuohua@qq.com>
2018-08-04 00:50:37 +0800

    20180804 晚十二点待续... hw-oj

--- 数据分类处理.py
+++ 数据分类处理.py
@@ -3,22 +3,50 @@
 
 while True:
     try:
-        R = input().split(" ")
         I = input().split(" ")
+        R = input().split(" ")
         R = R[1:]
+        R_value = R
+        R = list(enumerate(R))
+        # TODO: 先把R处理一下：按值从小到大排列，且剔除序号大的重复值
+        tmp_i = list()
+        tmp_r = list()
+        for m in range(len(R)):
+            # 去重
+            if R[m][1] not in tmp_i:
+                tmp_i.append(R[m][1])
+                tmp_r.append(R[m])
+        R = tmp_r
+        tmp = list()
+        for m in range(len(R)):
+            # 排序
+            if R[m][1] not in tmp:
+                if len(tmp) == 0:
+                    tmp.append(R[m])
+                else:
+                    for n in range(len(tmp)):
+                        if R[m][1] < tmp[n][1]:
+                            tmp.insert(n, R[m])
+                            break
+                        elif n == len(tmp)-1:
+                            tmp.append(R[m])
+        # TODO: 要不要重新赋值R？tmp已经排好序
+        R = tmp
         I = I[1:]
-        res = dict()
+        res = list()
         for i in range(len(R)):
-            count = 0
+            res_i = list()
+            count_i = 0
             for j in range(len(I)):
-                if R[i] in I[j]:
-                    print(R[i])
-                    count += 1
-                    res[I[j]] = j
-
-            print(count)
-            print(res[i])
-            print(I[res[i]])
-
+                if R[i][1] in I[j]:
+                    count_i += 1
+                    res_i.append(j)
+                    res_i.append(I[j])
+            if count_i != 0:
+                res.append(R[i][1])
+                res.append(count_i)
+                for p in res_i:
+                    res.append(p)
+        print(str(len(res))+" "+" ".join(res))
     except:
         break
