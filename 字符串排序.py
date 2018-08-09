8cc041fd3380d85b4edd2999b05ff83bf0743132
caozuohua <caozuohua@qq.com>
2018-08-10 00:13:34 +0800

    20180809 今天满分一次过了科二 晚十二点 hw-oj

--- 字符串排序.py
+++ 字符串排序.py
@@ -5,23 +5,21 @@ while True:
     try:
         # 根据字母表顺序给输入的字符串中的字符排序
         strings = input()
-        strings = list(strings)
-        # alphabet = [chr(n) for n in range(97, 123)]
+        # strings = list(strings)
+        # pure_string 是纯字母字符列表
+        pure_strings = [i for i in strings if i.isalpha()]
+        alphabet = [chr(n) for n in range(97, 123)]
         res = list()
-
+        # 先组织所有字母字符
+        for i in alphabet:
+            for j in pure_strings:
+                if j.lower() == i:
+                    res.append(j)
+        # 再插入非字母字符
         length = len(strings)
-        for i in range(length):
-            if not strings[i].isalpha():
-                continue
-            for j in range(length):
-                if not strings[j].isalpha():
-                    continue
-                if strings[i].lower() > strings[j].lower():
-                    strings[i], strings[j] = strings[j], strings[i]
-                # TODO: 解决大小写字符按原始顺序排序的问题
-                elif (strings[i].isupper() and strings[j].islower()) or (strings[i].islower() and strings[j].isupper()):
-
-
-        print(''.join(strings))
+        for k in range(length):
+            if not strings[k].isalpha():
+                res.insert(k, strings[k])
+        print(''.join(res))
     except:
         break
